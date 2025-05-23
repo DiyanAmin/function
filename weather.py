abb842603dc994de7c3d55bada1251a49e92803f
DiyanAmin <pakuptl3112@gmail.com>
2025-05-23 17:41:47 +0530

    completed

---
+++ weather.py
@@ -0,0 +1,7 @@
+#Program to display weather in autumn and spring
+def weather():
+    print('Wethaer is pleasant in',spring)
+    print('Weather is humid in',autumn)
+autumn=str(input('Weather?: '))
+spring=str(input('Weather?: '))
+weather()
